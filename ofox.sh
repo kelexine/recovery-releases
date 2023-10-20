92c70cdc939d958bf14d6462ece0f96fec916dde
kelexine <frankiekelechi@gmail.com>
2023-10-20 15:58:25 +0000

    added ofox setup

---
+++ ofox.sh
@@ -0,0 +1,17 @@
+GNU nano 6.2                                                       setup.sh
+#!/bin/bash
+
+sudo apt update && sudo apt -y upgrade && sudo apt -y install gperf gcc-multilib gcc-10-multilib g++-multilib g++-10-multilib libc6-dev lib32>
+
+mkdir ~/bin
+        curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+        chmod a+x ~/bin/repo
+        sudo ln -sf ~/bin/repo /usr/bin/repo
+
+git config --global user.name "kelexine"
+git config --global user.email "frankiekelechi@gmail.com"
+
+cd OrangeFox_sync/sync/
+./orangefox_sync.sh --branch 11.0 --path ~/OrangeFox_11.0
+
+echo "environment setup completed successfully,"
