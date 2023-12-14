017c3eb8f3b72b7de65540afb662dc560768c00b
kelexine <frankiekelechi@gmail.com>
2023-12-14 16:25:19 +0000

    updated twrp.sh

--- twrp.sh
+++ twrp.sh
@@ -9,6 +9,6 @@ mkdir ~/bin
 
 git config --global user.name "kelexine"
 git config --global user.email "frankiekelechi@gmail.com"
-mkdir twrp && cd twrp
+mkdir twrp; cd twrp
 echo "environment setup completed successfully,"
 
